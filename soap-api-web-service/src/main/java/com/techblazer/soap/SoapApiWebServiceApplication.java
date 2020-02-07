fde183d2f74863da222b67e8da971218c17504ab
promise <promiseokosun@gmail.com>
2020-02-07 17:01:51 +0100

    update on the soap api

--- soap-api-web-service/src/main/java/com/techblazer/soap/SoapApiWebServiceApplication.java
+++ soap-api-web-service/src/main/java/com/techblazer/soap/SoapApiWebServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SoapApiWebServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SoapApiWebServiceApplication.class, args);
+		SpringApplication.run(SoapApiWebServiceApplication.class, args); 
 	}
 
 }
